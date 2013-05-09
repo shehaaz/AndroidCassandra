70628c50d74b62b27b53d6ea5a00b6fbc013f00b
shehaaz <shehaaz@gmail.com>
2013-05-09 17:48:24 -0300

    refactored download data task

--- src/com/android/cassandra/droidbargain/feed/FeedActivity.java
+++ src/com/android/cassandra/droidbargain/feed/FeedActivity.java
@@ -59,82 +59,7 @@ public class FeedActivity extends ListActivity implements LocationListener {
 			userLng = "-73.580314"
 		    }
 		  
-		
-		
-
-		AsyncHttpClient client = new AsyncHttpClient();
-
-		client.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="+userLat+","+userLng+"&radius=500&types=book_store%7Cclothing_store%7Celectronics_store%7Cshoe_store%7Cjewelry_store&sensor=true&key=AIzaSyDMtyVzs_11fW_oye9hDLDu0OfPJXskBwg", 
-				new JsonHttpResponseHandler() {
-			@Override
-			public void onSuccess(JSONObject response) {
-
-				try {
-					JSONArray resultsArray = response.getJSONArray("results");
-					for(int i=0; i<resultsArray.length(); i++)
-					{
-						String name = resultsArray.getJSONObject(i).getString("name");
-						String id = resultsArray.getJSONObject(i).getString("id");
-						JSONObject geometry = resultsArray.getJSONObject(i).getJSONObject("geometry");
-						JSONObject location = geometry.getJSONObject("location");
-						Double lat = Double.parseDouble(location.getString("lat"));
-						Double lng = Double.parseDouble(location.getString("lng"));
-						String address = resultsArray.getJSONObject(i).getString("vicinity");
-						StoreFactory newStore = new StoreFactory(id,name,address,lat,lng);
-						//System.out.println(newStore.toString());
-						store_data.add(newStore);
-
-						AsyncHttpClient client = new AsyncHttpClient();
-						client.get("http://198.61.177.186:8080/virgil/data/android/posts/"+id,new AsyncHttpResponseHandler() {
-							@Override
-							public void onSuccess(String  response) {
-
-								if(response != null){
-
-									JSONObject jObject;
-									try {
-										jObject = new JSONObject(response);
-
-										Iterator<?> keys = jObject.keys();
-										while(keys.hasNext()){
-											String currentTimestamp = (String) keys.next();
-
-											String postString = jObject.getString(currentTimestamp);
-											JSONObject currentPostObject = new JSONObject(postString);
-
-											String title = currentPostObject.getString("title");
-											String desc = currentPostObject.getString("body"); 
-											String price = currentPostObject.getString("price");
-											String location = currentPostObject.getString("location");
-
-											System.out.println(title + " " +desc+ " " + price + " " + location );
-
-											feed_data.add(new FeedFactory(currentTimestamp, title, desc, price, location));	
-											
-										}
-										FeedAdapter adapter = new FeedAdapter(appContext,R.layout.feed_item,feed_data);
-										setListAdapter(adapter);
-									} catch (JSONException e) {
-										// TODO Auto-generated catch block
-										e.printStackTrace();
-									}
-								}
-
-							}
-						});
-						
-					}
-
-				}
-
-				catch (JSONException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-		});
-
-
+		downloadData();
 	}
 
 
@@ -188,6 +113,82 @@ public class FeedActivity extends ListActivity implements LocationListener {
 	  public void onProviderDisabled(String provider) {
 	    Toast.makeText(this, "Disabled provider " + provider,
 	        Toast.LENGTH_SHORT).show();
+	  }
+	  
+	  private void downloadData(){
+	  	
+	  	AsyncHttpClient googlePlaces_client = new AsyncHttpClient();
+
+		googlePlaces_client.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="+userLat+","+userLng+"&radius=500&types=book_store%7Cclothing_store%7Celectronics_store%7Cshoe_store%7Cjewelry_store&sensor=true&key=AIzaSyDMtyVzs_11fW_oye9hDLDu0OfPJXskBwg", 
+				new JsonHttpResponseHandler() {
+			@Override
+			public void onSuccess(JSONObject response) {
+
+				try {
+					JSONArray resultsArray = response.getJSONArray("results");
+					for(int i=0; i<resultsArray.length(); i++)
+					{
+						String name = resultsArray.getJSONObject(i).getString("name");
+						String id = resultsArray.getJSONObject(i).getString("id");
+						JSONObject geometry = resultsArray.getJSONObject(i).getJSONObject("geometry");
+						JSONObject location = geometry.getJSONObject("location");
+						Double lat = Double.parseDouble(location.getString("lat"));
+						Double lng = Double.parseDouble(location.getString("lng"));
+						String address = resultsArray.getJSONObject(i).getString("vicinity");
+						StoreFactory newStore = new StoreFactory(id,name,address,lat,lng);
+						//System.out.println(newStore.toString());
+						store_data.add(newStore);
+
+						AsyncHttpClient cassandra_client = new AsyncHttpClient();
+						cassandra_client.get("http://198.61.177.186:8080/virgil/data/android/posts/"+id,new AsyncHttpResponseHandler() {
+							@Override
+							public void onSuccess(String  response) {
+
+								if(response != null){
+
+									JSONObject jObject;
+									try {
+										jObject = new JSONObject(response);
+
+										Iterator<?> keys = jObject.keys();
+										while(keys.hasNext()){
+											String currentTimestamp = (String) keys.next();
+
+											String postString = jObject.getString(currentTimestamp);
+											JSONObject currentPostObject = new JSONObject(postString);
+
+											String title = currentPostObject.getString("title");
+											String desc = currentPostObject.getString("body"); 
+											String price = currentPostObject.getString("price");
+											String location = currentPostObject.getString("location");
+
+											System.out.println(title + " " +desc+ " " + price + " " + location );
+
+											feed_data.add(new FeedFactory(currentTimestamp, title, desc, price, location));	
+											
+										}
+										FeedAdapter adapter = new FeedAdapter(appContext,R.layout.feed_item,feed_data);
+										setListAdapter(adapter);
+									} catch (JSONException e) {
+										// TODO Auto-generated catch block
+										e.printStackTrace();
+									}
+								}
+
+							}
+						});
+						
+					}
+
+				}
+
+				catch (JSONException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
+	  	
 	  }
 
 }

92dd673387a6a42d3037181f55ad5a993cbd7f02
shehaaz <shehaaz@gmail.com>
2013-06-01 19:25:59 -0400

    Added Like Button

--- src/com/android/cassandra/droidbargain/feed/FeedAdapter.java
+++ src/com/android/cassandra/droidbargain/feed/FeedAdapter.java
@@ -8,10 +8,13 @@ import android.graphics.BitmapFactory;
 import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.cassandra.droidbargain.R;
 
@@ -34,7 +37,9 @@ public class FeedAdapter extends ArrayAdapter<DealFactory>{
 	public View getView(int position, View convertView, ViewGroup parent){
 		View row = convertView;
 		FeedHolder holder = null;
-
+		
+		DealFactory deal = feedFactory.get(position);
+		
 		if(row == null){
 			LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 			row = inflater.inflate(layoutResourceId, parent, false);
@@ -45,21 +50,42 @@ public class FeedAdapter extends ArrayAdapter<DealFactory>{
 			holder.price = (TextView) row.findViewById(R.id.feed_price);
 			holder.location = (TextView) row.findViewById(R.id.feed_location);
 			holder.user = (TextView) row.findViewById(R.id.feed_posted_by);
-
+			holder.thumbs_up = (ImageButton) row.findViewById(R.id.thumbs_up);
+						
 			row.setTag(holder);
+			holder.thumbs_up.setTag(holder);
 		}
 		else{
 			holder = (FeedHolder)row.getTag();
 		}
-		//For every item in the list. set Title, address and rating
-		DealFactory feed = feedFactory.get(position);
-		byte [] output = Base64.decode(feed.getImage(), Base64.DEFAULT);
+		
+		byte [] output = Base64.decode(deal.getImage(), Base64.DEFAULT);
 		Bitmap outputImage = BitmapFactory.decodeByteArray(output, 0, output.length);
+		
 		holder.image.setImageBitmap(outputImage);
-		holder.desc.setText(feed.getDesc());
-		holder.price.setText(feed.getPrice()+"$");
-		holder.location.setText(feed.getLocation());
-		holder.user.setText("Posted By: "+feed.getUsername());
+		holder.desc.setText(deal.getDesc());
+		holder.price.setText("Price: "+deal.getPrice());
+		holder.location.setText(deal.getLocation());
+		holder.user.setText("Posted By: "+deal.getUsername());
+		
+		holder.holder_timestamp = deal.getTimeStamp();
+		holder.holder_image =  deal.getImage();
+		holder.holder_desc = deal.getDesc();
+		holder.holder_price = deal.getPrice();
+		holder.holder_location = deal.getLocation();
+		holder.holder_name = deal.getUsername();
+		
+		
+		holder.thumbs_up.setOnClickListener(new OnClickListener() {
+			 
+			@Override
+			public void onClick(View v) {
+				
+				FeedHolder tempHolder = (FeedHolder) v.getTag();
+ 
+				Toast.makeText(context, tempHolder.holder_desc, Toast.LENGTH_SHORT).show();
+			}
+		});
 
 		return row;
 	}
@@ -70,6 +96,21 @@ public class FeedAdapter extends ArrayAdapter<DealFactory>{
 		TextView price;
 		TextView location;
 		TextView user;
+		ImageButton thumbs_up;
+		
+		String holder_timestamp;
+		String holder_image;
+		String holder_desc;
+		String holder_price;
+		String holder_location;
+		String holder_name;
 	}
+	
+	
+	
+    @Override
+    public boolean isEnabled(int position) {
+        return false;
+    }
 
 }
